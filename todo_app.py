e5ef124d9a8dd93ef99e7e78a9e67ba471c26aff
kumarh125 <hemantkumar3089@gmail.com>
2025-01-22 21:36:33 +0100

    Reapply "Merge pull request #1 from kumarh125/kumarh125-patch-1"
    This reverts commit fb4892af7efcd6d41f7936e730bdfd576ec45cea.

--- todo_app.py
+++ todo_app.py
@@ -1,66 +1,69 @@
 
 while True:
+    # get user input and remove space charaters from this
     user_choice = input("enter add, show, edit, complete or exit:")
     user_choice = user_choice.strip()
 
     match user_choice:
-        case 'add':           
-            
-            file = open('todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+        case 'add':     
+            #file = open('todos.txt', 'r')
+            #todos = file.readlines()
+            #file.close()
+            with open('todos.txt', 'r') as file:     #with as context manager, no need to close file
+                todos = file.readlines()
 
             user_input = input("enter a todo:") + "\n"
             todos.append(user_input)
 
-            file2 = open('todos.txt', 'w')
-            file2.writelines(todos)
-            file2.close()
+            #file2 = open('todos.txt', 'w')
+            #file2.writelines(todos)
+            #file2.close()
 
 
-        case 'show' | 'display':
-            file3 = open('todos.txt', 'r')
-            todos = file3.readlines()
-            file3.close()
-            
+        case 'show' | 'display':            
+            with open('todos.txt', 'r') as file:     #with as context manager, no need to close file
+                todos = file.readlines()   
+
             new_todos = [item.strip("\n") for item in todos]  #list comprehension
             todos = new_todos
-
-            for index, item in enumerate(todos):
-                item = item.title()
-                #item = item.strip('\n')
-                print(f"{index+1}-{item}")
-                #print(f"{index+1}-{item.strip('\n')}")
+            
+            for index, item in enumerate(todos):    #item = item.strip('\n')
+                item = item.title()                 
+                print(f"{index+1}-{item}")          #print(f"{index+1}-{item.strip('\n')}")         
+                                                    
 
         case 'edit':
-            file = open('todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+             with open('todos.txt', 'r') as file:     #with as context manager, no need to close file
+                todos = file.readlines()
 
-            number = int(input("enter the number to be edited:"))
-            number = number-1
-            todos[number] = input("enter a new to do")
-
-            file = open('todos.txt', 'w')
-            file.writelines(todos)
-            file.close()
+             number = int(input("enter the number to be edited:"))
+             number = number-1
+             todos[number] = input("enter a new to do") + "\n"
             
-        case 'complete':
-            file = open('todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+             with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+            
+            
+        case 'complete':            
+            with open('todos.txt', 'r') as file:     #with as context manager, no need to close file
+                todos = file.readlines()
 
             number = int(input("enter the number to be completed:"))
-            todos.pop(number-1)
+            donetask = todos.pop(number-1)
 
-            file = open("todos.txt", 'w')
-            file.writelines(todos)
-            file.close()
+            print(f"the item {donetask.strip("\n")} is now finished. well done")
+            
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
 
         case 'exit':
             break
+
+
         case _:
-            print("wrong comand")             
+            print("wrong comand")    
+
+
 print("bye!")
 
     
